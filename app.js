e7d0241a63c95d3c2edf055b54c4de59b478f15a
Owen Klumb <512WebDev@gmail.com>
2017-05-14 11:39:54 +0900

    From, cards added

--- app.js
+++ app.js
@@ -2,9 +2,14 @@ $(document).ready(function(){
 
 /*NavBar Javascript*/
 $(".dropdown-button").dropdown({ hover: false, belowOrigin: true, alignment: 'right' });
+$(".button-collapse").sideNav();
+
+/*Buttons Javascript*/
 
 
-
+/*Forms Javascript*/
+$('select').material_select();
+ 
 
 
 });
